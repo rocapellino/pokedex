f2af3e89bb0894b0429c7eaa882f99f8190afa4d
rocapellino <ro.capellino@gmail.com>
2026-09-03 15:47:32 -0300

    fix(terraform): remover enable_non_ssl_port en azurerm_redis_cache para compatibilidad azurerm v4+

--- infra/terraform/modules/azure/database/main.tf
+++ infra/terraform/modules/azure/database/main.tf
@@ -46,7 +46,6 @@ resource "azurerm_redis_cache" "redis" {
   capacity            = 0
   family              = "C"
   sku_name            = "Basic"
-  enable_non_ssl_port = false
   minimum_tls_version = "1.2"
 
   redis_configuration {
