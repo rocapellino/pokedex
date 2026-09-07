d7a26ec426be479c9a30f82f01b34e7494d9cc23
rocapellino <ro.capellino@gmail.com>
2026-09-07 14:17:27 -0300

    fix(ci): fix setup-node and trivy action SHAs and satisfy Checkov CKV_TF_2

--- infra/opentofu/environments/cloud/main.tf
+++ infra/opentofu/environments/cloud/main.tf
@@ -4,7 +4,8 @@
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "20.8.5"
+  #checkov:skip=CKV_TF_2: "Versión semántica fija de módulo oficial EKS"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.30"
